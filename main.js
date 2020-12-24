fe0cdaf518817812c4ee19ef0ff846faed106144
tdramirez <tdramirez@est.itsgg.edu.ec>
2020-12-24 10:51:19 -0500

    tercer commit 2020

--- main.js
+++ main.js
@@ -1,8 +1,9 @@
 const opcion = {
 	strings: ['Feliz Navidad Putos Amos'],
-	typeSpeed: 150,
+	typeSpeed: 200,
+	backSpeed: 100,
 	cursorChar: '_',
-	// loop: true,
+	loop: true,
 }
 
 const typed6 = new Typed('#texto', opcion)
